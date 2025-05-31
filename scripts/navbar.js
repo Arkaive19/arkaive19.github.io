6771d8fe39ea7421b6612a4a60cb82e779b2be6d
Aarambha Pyakurel <arkaive19@gmail.com>
2025-05-31 22:24:45 +0545

    Finalized Side Items

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,24 +1,23 @@
-let lastScrollTop = 0;
-const navbar = document.querySelector(".nav-bar");
-window.addEventListener("scroll", function () {
-  const currentScroll =
-    window.pageYOffset || document.documentElement.scrollTop;
+let lastScrollTop = 0
+const navbar = document.querySelector('.nav-bar')
+window.addEventListener('scroll', function () {
+  const currentScroll = window.pageYOffset || document.documentElement.scrollTop
 
   // Scroll down
   if (currentScroll > lastScrollTop) {
-    navbar.style.top = "-100px"; // Hide navbar
+    navbar.style.top = '-100px' // Hide navbar
   }
   // Scroll up
   else {
-    navbar.style.top = "0"; // Show navbar
+    navbar.style.top = '0' // Show navbar
   }
 
   // Add shadow if not at top
   if (currentScroll > 0) {
-    navbar.classList.add("shadow");
+    navbar.classList.add('shadow')
   } else {
-    navbar.classList.remove("shadow");
+    navbar.classList.remove('shadow')
   }
 
-  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Prevent negative values
-});
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll // Prevent negative values
+})
