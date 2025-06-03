b3aff75660bc91c11cec3a766db6e28181666cf2
Aarambha Pyakurel <arkaive19@gmail.com>
2025-06-03 15:14:41 +0545

    added a effect

---
+++ scripts/titleCard.js
@@ -0,0 +1,13 @@
+let title = document.querySelector('title')
+setInterval(() => {
+  title.innerText = 'A'
+  title.innerText = 'Ar'
+  title.innerText = 'Ark'
+  title.innerText = 'Arka'
+  title.innerText = 'Arkai'
+  title.innerText = 'Arkaiv'
+  title.innerText = 'Arkaive'
+  title.innerText = 'Arkaive1'
+  title.innerText = 'Arkaive19'
+  title.innerText = 'A'
+}, 1000)
