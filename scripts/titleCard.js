b21b91cf674071f70b4c8df6ce8d327355a09906
Aarambha Pyakurel <arkaive19@gmail.com>
2025-06-03 21:50:28 +0545

    Implemented New Title changes and added animation to elements

--- scripts/titleCard.js
+++ scripts/titleCard.js
@@ -1,13 +1,76 @@
 let title = document.querySelector('title')
-setInterval(() => {
-  title.innerText = 'A'
-  title.innerText = 'Ar'
-  title.innerText = 'Ark'
-  title.innerText = 'Arka'
-  title.innerText = 'Arkai'
-  title.innerText = 'Arkaiv'
-  title.innerText = 'Arkaive'
-  title.innerText = 'Arkaive1'
-  title.innerText = 'Arkaive19'
-  title.innerText = 'A'
-}, 1000) //effect
+    setTimeout(() => {
+      title.innerText = 'A'
+    }, 1000)
+    setTimeout(() => {
+      title.innerText = 'Ar'
+    }, 2000)
+    setTimeout(() => {
+      title.innerText = 'Ark'
+    }, 3000)
+    setTimeout(() => {
+      title.innerText = 'Arka'
+    }, 4000)
+    setTimeout(() => {
+      title.innerText = 'Arkai'
+    }, 5000)
+    setTimeout(() => {
+      title.innerText = 'Arkaiv'
+    }, 6000)
+    setTimeout(() => {
+      title.innerText = 'Arkaive'
+    }, 7000)
+    setTimeout(() => {
+      title.innerText = 'Arkaive1'
+    }, 8000)
+    setTimeout(() => {
+      title.innerText = 'Arkaive19'
+    }, 9000)
+    setTimeout(() => {
+      title.innerText = 'Arkaive1'
+    }, 10000)
+    setTimeout(() => {
+      title.innerText = 'Arkaive'
+    }, 11000)
+    setTimeout(() => {
+      title.innerText = 'Arkaiv'
+    }, 12000)
+    setTimeout(() => {
+      title.innerText = 'Arkai'
+    }, 13000)
+    setTimeout(() => {
+      title.innerText = 'Arka'
+    }, 14000)
+    setTimeout(() => {
+      title.innerText = 'Ark'
+    }, 15000)
+    setTimeout(() => {
+      title.innerText = 'Ar'
+    }, 16000)
+    setTimeout(() => {
+      title.innerText = 'A'
+    }, 17000)
+    setTimeout(() => {
+      title.innerText = 'Ar'
+    }, 18000)
+    setTimeout(() => {
+      title.innerText = 'Ark'
+    }, 19000)
+    setTimeout(() => {
+      title.innerText = 'Arka'
+    }, 20000)
+    setTimeout(() => {
+      title.innerText = 'Arkai'
+    }, 21000)
+    setTimeout(() => {
+      title.innerText = 'Arkaiv'
+    }, 22000)
+    setTimeout(() => {
+      title.innerText = 'Arkaive'
+    }, 23000)
+    setTimeout(() => {
+      title.innerText = 'Arkaive1'
+    }, 24000)
+    setTimeout(() => {
+      title.innerText = 'Arkaive19'
+    }, 25000)
