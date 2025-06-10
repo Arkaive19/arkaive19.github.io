8453ce9efd561ea450b235481b6d8c88053d3602
Aarambha Pyakurel <arkaive19@email.com>
2025-06-10 22:12:16 +0545

    Added Comments

--- scripts/hashAnchor.js
+++ scripts/hashAnchor.js
@@ -2,3 +2,4 @@ const baseUrl = window.location.origin + window.location.pathname
 window.location.hash
   ? window.location.replace(baseUrl)
   : console.log('Internal Error')
+//prevents hash based anchor to take effect
