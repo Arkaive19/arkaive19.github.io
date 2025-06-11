33c1f3f492d36fde146f3123671b01d01d4e3f59
Aarambha Pyakurel <arkaive19@email.com>
2025-06-11 22:56:38 +0545

    Implemented Simple Timeline Outline

--- scripts/TOS-corrector.js
+++ scripts/TOS-corrector.js
@@ -1,10 +1,12 @@
-const tos = document.querySelector('.TOS')
-document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    tos.style.animation =
-      'fade-up-in 2s cubic-bezier(0.77, 0, 0.175, 1) forwards'
-  }, 3200)
-  setTimeout(() => {
-    tos.style.animation = ''
-  }, 5200)
+const hov = document.querySelectorAll('.hov')
+hov.forEach((e) => {
+  document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+      e.style.animation =
+        'fade-up-in 2s cubic-bezier(0.77, 0, 0.175, 1) forwards'
+    }, 3200)
+    setTimeout(() => {
+      e.style.animation = ''
+    }, 5200)
+  })
 })
