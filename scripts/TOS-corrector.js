006a24ae4e1374cbad05d04e359f10c5d9a51ea3
Aarambha Pyakurel <arkaive19@email.com>
2025-07-03 15:48:52 +0545

    Added Changes To Project Section

--- scripts/TOS-corrector.js
+++ scripts/TOS-corrector.js
@@ -10,3 +10,16 @@ hov.forEach((e) => {
     }, 5200)
   })
 })
+
+const hovv = document.querySelectorAll('.hovv')
+hovv.forEach((e) => {
+  document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+      e.style.animation =
+        'fade-up-in 2s cubic-bezier(0.77, 0, 0.175, 1) forwards'
+    }, 3200)
+    setTimeout(() => {
+      e.style.animation = ''
+    }, 5200)
+  })
+})
